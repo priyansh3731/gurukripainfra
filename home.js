24337d8c73216889152e8a6332b9d0374e107331
priyansh3731 <118509082+priyansh3731@users.noreply.github.com>
2023-08-08 01:14:08 +0530

    debug some bug

--- home.js
+++ home.js
@@ -4,8 +4,14 @@ let sections = document.querySelectorAll("section")
 window.onscroll = () =>{
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop-200;
+        let offset = sec.offsetTop;
         let height = sec.offsetHeight;
+
+        if(window.innerWidth<600){
+            offset = sec.offsetTop-800;
+        }else{
+            offset = sec.offsetTop-200;
+        }
 
         if(top>=offset && top < offset+height){
             sec.classList.add("show-animate");
