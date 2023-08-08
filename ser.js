e7c583411a403c10b4b2f98a2884b3f30e076786
priyansh3731 <118509082+priyansh3731@users.noreply.github.com>
2023-08-08 10:11:50 +0530

    debug some bug

---
+++ ser.js
@@ -0,0 +1,22 @@
+let sections = document.querySelectorAll("section")
+
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+
+        if(window.innerWidth<600){
+            offset = sec.offsetTop-500;
+        }else{
+            offset = sec.offsetTop-200;
+        }
+
+        if(top>=offset && top < offset+height){
+            sec.classList.add("show-animate");
+        }else{
+            sec.classList.remove("show-animate");
+        }
+    })
+}
