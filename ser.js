4b2c0e1abe6761dc0272951d4740e4a2122e772c
priyansh3731 <118509082+priyansh3731@users.noreply.github.com>
2023-08-08 10:18:53 +0530

    debug some bug

--- ser.js
+++ ser.js
@@ -8,7 +8,7 @@ window.onscroll = () =>{
         let height = sec.offsetHeight;
 
         if(window.innerWidth<600){
-            offset = sec.offsetTop-1000;
+            offset = sec.offsetTop-500;
         }else{
             offset = sec.offsetTop-200;
         }
