8ac8f7eb0f4b9df555968c9dddc70a1044db5ca2
priyansh3731 <118509082+priyansh3731@users.noreply.github.com>
2023-08-20 13:05:52 +0530

    add some page

--- review.js
+++ review.js
@@ -4,13 +4,13 @@ let sections = document.querySelectorAll("section")
 window.onscroll = () =>{
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop-400;
+        let offset = sec.offsetTop;
         let height = sec.offsetHeight;
+
+        offset = sec.offsetTop-350;
 
         if(top>=offset && top < offset+height){
             sec.classList.add("show-animate");
-        }else{
-            sec.classList.remove("show-animate");
         }
     })
 }
